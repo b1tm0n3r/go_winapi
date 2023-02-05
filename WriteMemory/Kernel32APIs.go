c68673460ca8612e0160c2d7231b62fd6f7b4209
MkTech-0-8YT3 <mktech@protonmail.com>
2023-02-05 20:05:31 +0100

    Add full WriteProcessMemory impl with validation

--- WriteMemory/Kernel32APIs.go
+++ WriteMemory/Kernel32APIs.go
@@ -2,8 +2,86 @@ package main
 
 import (
 	"syscall"
+	"unsafe"
 )
 
 var (
-	kernel32_DLL = syscall.NewLazyDLL("kernel32.dll")
+	kernel32_DLL           = syscall.NewLazyDLL("kernel32.dll")
+	procOpenProcess        = kernel32_DLL.NewProc("OpenProcess")
+	procGetCurrentProcess  = kernel32_DLL.NewProc("GetCurrentProcess")
+	procVirtualAllocEx     = kernel32_DLL.NewProc("VirtualAllocEx")
+	procWriteProcessMemory = kernel32_DLL.NewProc("WriteProcessMemory")
+	procReadProcessMemory  = kernel32_DLL.NewProc("ReadProcessMemory")
+	procCloseHandle        = kernel32_DLL.NewProc("CloseHandle")
 )
+
+// Base: https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-openprocess
+func OpenProcess(dwDesiredAccess int32, bInheritHandle bool, dwProcessId uint32) uintptr {
+	inherit_boolToUint := 0
+	if bInheritHandle == true {
+		inherit_boolToUint = 1
+	}
+
+	ret, _, _ := procOpenProcess.Call(
+		uintptr(dwDesiredAccess),
+		uintptr(inherit_boolToUint),
+		uintptr(dwProcessId))
+
+	return ret // Return should be non-null (returns process handle)
+}
+
+// Base: https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-getcurrentprocessid
+func GetCurrentProcess() uintptr {
+	ret, _, _ := procGetCurrentProcess.Call()
+
+	return ret // Return should be non-null (returns process handle)
+}
+
+// Base: https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualallocex
+func VirtualAllocEx(hProcess uintptr, lpAddress uintptr, dwSize uint32,
+	flAllocationType uint32, flProtect uint32) uintptr {
+
+	ret, _, _ := procVirtualAllocEx.Call(
+		hProcess,
+		lpAddress,
+		uintptr(dwSize),
+		uintptr(flAllocationType),
+		uintptr(flProtect))
+
+	return ret // return value should be non-null
+}
+
+// Base: https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-writeprocessmemory
+func WriteProcessMemory(hProcess uintptr, lpBaseAddress uintptr, lpBuffer []byte,
+	nSize uint32, lpNumberOfBytesWritten *uintptr) uint32 {
+
+	ret, _, _ := procWriteProcessMemory.Call(
+		hProcess,
+		lpBaseAddress,
+		uintptr(unsafe.Pointer(&lpBuffer[0])),
+		uintptr(nSize),
+		uintptr(unsafe.Pointer(lpNumberOfBytesWritten)))
+
+	return uint32(ret) // return value should be non-zero
+}
+
+// Base: https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-readprocessmemory
+func ReadProcessMemory(hProcess uintptr, lpBaseAddress uintptr, lpBuffer []byte,
+	nSize uint32, lpNumberOfBytesRead *uintptr) bool {
+
+	ret, _, _ := procReadProcessMemory.Call(
+		hProcess,
+		lpBaseAddress,
+		uintptr(unsafe.Pointer(&lpBuffer[0])),
+		uintptr(nSize),
+		uintptr(unsafe.Pointer(lpNumberOfBytesRead)))
+
+	return uint32(ret) != 0 // return value should be non-zero
+}
+
+// Base: https://learn.microsoft.com/en-us/windows/win32/api/handleapi/nf-handleapi-closehandle
+func CloseHandle(hObject uintptr) bool {
+	ret, _, _ := procCloseHandle.Call(uintptr(unsafe.Pointer(&hObject)))
+
+	return uint(ret) == 0
+}
--- WriteMemory/main.go
+++ WriteMemory/main.go
@@ -1,15 +1,55 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"os"
+	"syscall"
 )
 
 func main() {
 
-	fmt.Println("[i] WriteMemory usage")
-	if len(os.Args) != 2 {
+	fmt.Println("[i] WriteMemory")
 
+	fmt.Println("[+] Opening handle to current process...")
+
+	hProcess := GetCurrentProcess()
+	if hProcess == 0 {
+		fmt.Println("[!] Could not obtain current process handle, aborting...")
+		return
 	}
+	fmt.Printf("[+] Obtained process handle.\n")
+
+	memAddr := VirtualAllocEx(hProcess, 0, 1024, (0x1000 | 0x2000), syscall.PAGE_EXECUTE_READWRITE)
+	if memAddr == 0 {
+		fmt.Printf("[!] Error allocating memory - returned value: %d\n", memAddr)
+		return
+	}
+	fmt.Printf("[+] Successfully allocated memory at addr: 0x%x\n", memAddr)
+
+	//fmt.Println("[dbg] addr ptr loc: ", memAddr)
+	var valToWrite []byte = []byte{0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90}
+	var writtenSize uintptr = 0
+	memWriteRes := WriteProcessMemory(hProcess, memAddr, valToWrite, uint32(len(valToWrite)), &writtenSize)
+	if memWriteRes == 0 {
+		fmt.Printf("[!] Error writing bytes: <%x> to address: <0x%x> in current process. Written size: %d, Returned value: %d\n", valToWrite, memAddr, writtenSize, memWriteRes)
+		return
+	}
+	fmt.Printf("[+] Written %d bytes under address: 0x%x\n", writtenSize, memAddr)
+
+	fmt.Printf("[+] Validating - Read from address into buffer...\n")
+	var bytesFromMemory []byte = []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+	var readSize uintptr = 0
+	memReadRes := ReadProcessMemory(hProcess, memAddr, bytesFromMemory, uint32(len(valToWrite)), &readSize)
+	if !memReadRes {
+		fmt.Printf("[!] Error read bytes: <%s> from address: <0x%x> in current process. Read size: %d, Returned value: %d\n", bytesFromMemory, memAddr, readSize, memReadRes)
+		return
+	}
+	fmt.Printf("[+] Read %d bytes under address: 0x%x\n", readSize, memAddr)
+
+	if bytes.Equal(valToWrite, bytesFromMemory) {
+		fmt.Println("[+] Write/Read arrays store the same value\n")
+	}
+
+	CloseHandle(hProcess)
 
 }
